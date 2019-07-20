db0e5400d30decdb7973826377468aacfc450767
victortobias <victor.tobias1@gmail.com>
2019-07-20 13:29:38 -0300

    Inserção do Ajax no controller.js

--- controller.js
+++ controller.js
@@ -20,18 +20,67 @@ $( ".lamp" ).mouseleave(function() {
 
 //controle da iluminação
 var lamps = [0,0,0,0,0];
+var signal;
 $(".lamp, .lamp3, .lamp2").on('click', function(e, x){
 	id = $(this).attr('value');
 	if(lamps[id-1] == 0){
-		//gatilho para ligar
-		lamps[id-1] = 1
-		name = '.lamp'+id.toString()
-		$(name).addClass('on');
+		call = callEsp(id, 1)
+		if(call == false){
+			console.log('Houve erro na comunicação com o ESP')
+		}else if(call[0] == true){
+			lamps[id-1] = 1
+			callEsp(lamps[id-1])
+			name = '.lamp'+id.toString()
+			$(name).addClass('on');
+			console.log('Setor '+id+' ligado')
+		}else{
+			console.log('Erro desconhecido abaixo:')
+			console.log(call)
+		}
 	}else{
-		//gatilho para desligar
-		lamps[id-1] = 0;
-		name = '.lamp'+id.toString()
-		$(name).removeClass('on');
+		call = callEsp(id, 0)
+		if(call == false){
+			console.log('Houve erro na comunicação com o ESP')
+		}else if(call[0] == true){
+			lamps[id-1] = 0
+			callEsp(lamps[id-1])
+			name = '.lamp'+id.toString()
+			$(name).removeClass('on');
+			console.log('Setor '+id+' desligado')
+		}else{
+			console.log('Erro desconhecido abaixo:')
+			console.log(call)
+		}
 	}
 	console.log(lamps)
 })
+
+function callEsp(sector, signal){
+	/*  SIMULADOR DO ESP RESPONDENDO
+	if(signal == 0){
+		return [true, 0];
+	}else{
+		return [true, 1];
+	}
+	*/
+
+	$.ajax({
+	  url : "IP DO ESP",
+	  type : 'GET',
+	  data : {
+	       signal: signal,
+	       sector: sector,
+	  },
+	})
+	.done(function(msg){
+		console.log(msg)
+	  response = [true, msg]
+	  return response;
+	})
+	.error(funtion(msg){
+
+	})
+	.fail(function(jqXHR, textStatus, msg){
+	  return false;
+	}); 
+}
